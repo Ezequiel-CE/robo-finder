524b62bb60f0ec33b7b815e2698713c53468a06f
eze car <ezequielce490@gmail.com>
2020-10-27 00:49:08 -0300

    Updates

---
+++ precache-manifest.4df0c488d0d765f654e2c1e6f3f4057a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db81f157a78036c6536ccf8077a58be1",
+    "url": "/robo-finder/index.html"
+  },
+  {
+    "revision": "f35a6adfc7d6b453000e",
+    "url": "/robo-finder/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3f9d35b7d06f618eeeb0",
+    "url": "/robo-finder/static/css/main.6f086235.chunk.css"
+  },
+  {
+    "revision": "f35a6adfc7d6b453000e",
+    "url": "/robo-finder/static/js/2.d2507867.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/robo-finder/static/js/2.d2507867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9d35b7d06f618eeeb0",
+    "url": "/robo-finder/static/js/main.7af57df2.chunk.js"
+  },
+  {
+    "revision": "b68ec0a044fc1af11916",
+    "url": "/robo-finder/static/js/runtime-main.cb1eb03c.js"
+  },
+  {
+    "revision": "4197aed63a267f9ae61f800cda823803",
+    "url": "/robo-finder/static/media/Pokemon Solid.4197aed6.woff"
+  }
+]);
